4c9b587c763929c28c78bf0b13ef09258ac8ea3d
Sodiq 'Ade' Sanusi <ademilddiq@gmail.com>
2024-12-05 10:26:14 +0100

    setup: adding styling variables and fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
 import "@/styles/globals.css";
+import { Yeseva_One, Lato } from "next/font/google";
+
+const yeseva_one = Yeseva_One({
+  subsets: ["latin"],
+  weight: ["400"],
+  display: "swap",
+})
+
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+  display: "swap",
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${lato.style.fontFamily};
+        }
+        h1, h2 {
+          font-family: ${yeseva_one.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
