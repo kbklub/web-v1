744eff046c8c4786b531889cc470f0481ce6859e
Sodiq 'Ade' Sanusi <ademilddiq@gmail.com>
2024-12-06 23:46:37 +0100

    feat: disable scroll on mobile menu, implement fixed overflow mobile menu

--- components/NavBar/index.jsx
+++ components/NavBar/index.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import styles from "./NavBar.module.css";
 import kbLogo from "../../assets/images/kb_klub_logo.png";
 import { HiArrowSmallRight, HiBars3, HiMiniChevronDown, HiMiniXMark } from "react-icons/hi2";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const NavBar = () => {
@@ -12,13 +12,21 @@ const NavBar = () => {
   const [dropAbout, setDropAbout] = useState(false);
   const [dropArms, setDropArms] = useState(false);
 
-  const handleMobileMenu = () => {
-    setMobileMenu(!isMobileMenu);
-  }
+  useEffect(() => {
+    if (isMobileMenu) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
 
-  const handleDropdown = (option) => {
-    
-  }
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [isMobileMenu])
+
+  // const handleDropdown = (option) => {
+
+  // }
 
   return (
     <nav className={styles.navLayout}>
@@ -28,63 +36,65 @@ const NavBar = () => {
           <p>Kb klub</p>
         </Link>
 
-        <button className={styles.hamburgerBtn} onClick={handleMobileMenu}>
+        <button className={styles.hamburgerBtn} onClick={() => setMobileMenu(!isMobileMenu)}>
           {!isMobileMenu ? <HiBars3 /> : ""}
           {isMobileMenu ? <HiMiniXMark /> : ""}
         </button>
       </div>
       {isMobileMenu && (
         <div className={styles.mobileNavLayout}>
-          <ul className={styles.mobileNavContainer}>
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <button
-                className={dropAbout ? styles.dropButton: ""}
-                onClick={() => setDropAbout(!dropAbout)}
-              >
-                About Us
-                <HiMiniChevronDown />
-              </button>
-              <div className={!dropAbout ? styles.hiddenDropDown: ""}>
-                <ul className={styles.navSubList}>
-                  <li><Link href="/about">About KB KLUB</Link></li>
-                  <li><Link href="/about/kbites">Executives &amp; Kbites</Link></li>
-                  <li><Link href="/about/lifekbites">Life Kbites</Link></li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <Link href="/events">Events</Link>
-            </li>
-            <li>
-              <Link href="/editorial">Editorial</Link>
-            </li>
-            <li>
-              <button
-                className={dropArms ? styles.dropButton: ""}
-                onClick={() => setDropArms(!dropArms)}
-              >
-                Arms
-                <HiMiniChevronDown />
-              </button>
-              <div className={!dropArms ? styles.hiddenDropDown: ""}>
-                <ul className={styles.navSubList}>
-                  <li><Link href="/arms/philantrophy">Philanthropy</Link></li>
-                  <li><Link href="/arms/academic-excellence">Academic Excellence</Link></li>
-                  <li><Link href="/arms/social-empowerment">Social Empowerment</Link></li>
-                </ul>
-              </div>
-            </li>
-          </ul>
+          <div className={styles.mobileNavOverflow}>
+            <ul className={styles.mobileNavContainer}>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <button
+                  className={dropAbout ? styles.dropButton : ""}
+                  onClick={() => setDropAbout(!dropAbout)}
+                >
+                  About Us
+                  <HiMiniChevronDown />
+                </button>
+                <div className={!dropAbout ? styles.hiddenDropDown : ""}>
+                  <ul className={styles.navSubList}>
+                    <li><Link href="/about">About KB KLUB</Link></li>
+                    <li><Link href="/about/kbites">Executives &amp; Kbites</Link></li>
+                    <li><Link href="/about/lifekbites">Life Kbites</Link></li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <Link href="/events">Events</Link>
+              </li>
+              <li>
+                <Link href="/editorial">Editorial</Link>
+              </li>
+              <li>
+                <button
+                  className={dropArms ? styles.dropButton : ""}
+                  onClick={() => setDropArms(!dropArms)}
+                >
+                  Arms
+                  <HiMiniChevronDown />
+                </button>
+                <div className={!dropArms ? styles.hiddenDropDown : ""}>
+                  <ul className={styles.navSubList}>
+                    <li><Link href="/arms/philantrophy">Philanthropy</Link></li>
+                    <li><Link href="/arms/academic-excellence">Academic Excellence</Link></li>
+                    <li><Link href="/arms/social-empowerment">Social Empowerment</Link></li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
 
-          <Link href="/support-us" className={styles.partnerBtn}>
-            <button>
-              Partner with Us
-              <HiArrowSmallRight />
-            </button>
-          </Link>
+            <Link href="/support-us" className={styles.partnerBtn}>
+              <button>
+                Partner with Us
+                <HiArrowSmallRight />
+              </button>
+            </Link>
+          </div>
         </div>
       )}
     </nav>
