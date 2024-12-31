2f9f5e6d0c1475207e9662f6327c1e73adb03b03
Sodiq 'Ade' Sanusi <ademilddiq@gmail.com>
2024-12-31 15:39:21 +0100

    seo: adding the right SEO image

--- components/SEO/index.jsx
+++ components/SEO/index.jsx
@@ -29,21 +29,21 @@ const SEO = ({ pageDetails }) => {
         <meta property="og:description" content={seoDetails.description} />
         <meta property="og:type" content="website" />
         <meta property="og:url" content={seoDetails.url} />
-        <meta property="og:image" content="https://kbklub.org/images/seoImage.jpeg" />
+        <meta property="og:image" content="https://kbklub.org/images/seoImage.jpg" />
         <meta property="og:image:type" content="image/jpeg" />
         <meta property="og:image:alt" content="The KB Klub" />
-        <meta property="og:image:width" content="680" />
-        <meta property="og:image:height" content="674" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
 
         <meta name="twitter:title" content={seoDetails.title} />
         <meta name="twitter:description" content={seoDetails.description} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:url" content={seoDetails.url} />
-        <meta name="twitter:image" content="https://kbklub.org/images/seoImage.jpeg" />
+        <meta name="twitter:image" content="https://kbklub.org/images/seoImage.jpg" />
         <meta name="twitter:image:type" content="image/jpeg" />
         <meta name="twitter:image:alt" content="The KB Klub" />
-        <meta name="twitter:image:width" content="680" />
-        <meta name="twitter:image:height" content="674" />
+        <meta name="twitter:image:width" content="1200" />
+        <meta name="twitter:image:height" content="630" />
         <meta name="twitter:site" content="@KBKLUB" />  
       </Head>
     </>
