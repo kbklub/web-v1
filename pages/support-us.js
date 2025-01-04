6f2ea235c26716eca297bc90a14974898933fa3c
Sodiq 'Ade' Sanusi <ademilddiq@gmail.com>
2025-01-04 01:16:51 +0100

    responsive: work on the support us page

--- pages/support-us.js
+++ pages/support-us.js
@@ -18,83 +18,85 @@ const SupportUs = () => {
   return (
     <>
       <SEO pageDetails={pageSeo}/>
-      <header className={styles.headerLayout}>
-        <NavBar />
-        <div className={styles.headerContainer}>
-          <h1>Join Us in Making a Difference</h1>
-          <div className={styles.headerDetails}>
-            <p>
-              Our impactful philanthropy, social excellence, and academic excellence
-              projects thrive with the generous support and partnership from individuals,
-              companies, and organizations. Whether you&apos;re an individual, organization,
-              or company looking to support our mission or partner with us for impactful
-              events, we&apos;re here to collaborate. 
-            </p>
-            <p>
-              You can choose to support us by directly donating to our account or reach out
-              by filling the form on this page to know more about our partnership and event
-              proposals.
-            </p>
-          </div>
-        </div>
-      </header>
-      <main className={styles.supportLayout}>
-        <div className={styles.supportContainer}>
-          <div className={styles.accountContainer}>
-            <h3>Our Account Details:</h3>
-            <div className={styles.accountDetails}>
-              <p><b>Account Name:</b> KB Klub</p>
-              <p><b>Account Number:</b> 0014324124</p>
-              <p><b>Bank Name:</b> Guaranty Trust Bank (GTB)</p>
+      <div className={styles.bgWrapper}>
+        <header className={styles.headerLayout}>
+          <NavBar />
+          <div className={styles.headerContainer}>
+            <h1>Join Us in Making a Difference</h1>
+            <div className={styles.headerDetails}>
+              <p>
+                Our impactful philanthropy, social excellence, and academic excellence
+                projects thrive with the generous support and partnership from individuals,
+                companies, and organizations. Whether you&apos;re an individual, organization,
+                or company looking to support our mission or partner with us for impactful
+                events, we&apos;re here to collaborate. 
+              </p>
+              <p>
+                You can choose to support us by directly donating to our account or reach out
+                by filling the form on this page to know more about our partnership and event
+                proposals.
+              </p>
             </div>
           </div>
+        </header>
+        <main className={styles.supportLayout}>
+          <div className={styles.supportContainer}>
+            <div className={styles.accountContainer}>
+              <h3>Our Account Details:</h3>
+              <div className={styles.accountDetails}>
+                <p><b>Account Name:</b> KB Klub</p>
+                <p><b>Account Number:</b> 0014324124</p>
+                <p><b>Bank Name:</b> Guaranty Trust Bank (GTB)</p>
+              </div>
+            </div>
 
-          <form 
-            className={styles.supportFormLayout}
-            method="POST" action="https://formsubmit.co/2e4936a9576cc1f6b8f408e29ab94702"
-          >
-            <div className={styles.supportFormContainer}>
-              <div className={styles.supportInput}>
-                <label htmlFor="supportName">Name:</label>
-                <input
-                  type="text" id="supportName" name="name" required
-                  placeholder="Name of Organization, Company, Brand etc"
-                  value={supportName} onChange={(e) => setSupportName(e.target.value)}
-                />
+            <form 
+              className={styles.supportFormLayout}
+              method="POST" action="https://formsubmit.co/2e4936a9576cc1f6b8f408e29ab94702"
+            >
+              <div className={styles.supportFormContainer}>
+                <div className={styles.supportInput}>
+                  <label htmlFor="supportName">Name:</label>
+                  <input
+                    type="text" id="supportName" name="name" required
+                    placeholder="Name of Organization, Company, Brand etc"
+                    value={supportName} onChange={(e) => setSupportName(e.target.value)}
+                  />
+                </div>
+                <div className={styles.supportInput}>
+                  <label htmlFor="supportEmail">Email Address:</label>
+                  <input
+                    type="email" id="supportEmail" name="email" required
+                    placeholder="Email address of Organization, Company, Brand etc"
+                    value={supportEmail} onChange={(e) => setSupportEmail(e.target.value)}
+                  />
+                </div>
+                <div className={styles.supportTextArea}>
+                  <label htmlFor="supportMessage">Message/Description:</label>
+                  <textarea
+                    id="supportMessage" name="message" required
+                    placeholder="E.g We will like to partner with the Klub on abcd event and we will like to receive the proposal for the event."
+                    value={supportMessage} onChange={(e) => setSupportMessage(e.target.value)}
+                  >
+                  </textarea>
+                </div>
+                <div aria-hidden="true">
+                  <input type="hidden" name="_subject" value="Message from Contact Form on KB Website" />
+                  <input type="hidden" name="_autoresponse"
+                    value="Your message has been delivered to the KB Klub team. We will get back to you as soon as possible. Thank you for reaching out to us." 
+                  />
+                  <input type="hidden" name="_next" value="https://kbklub.org/support-us/" />
+                  <input type="hidden" name="_template" value="box" />
+                </div>
+                <button type="submit">
+                  Submit
+                  <FaArrowRight />
+                </button>
               </div>
-              <div className={styles.supportInput}>
-                <label htmlFor="supportEmail">Email Address:</label>
-                <input
-                  type="email" id="supportEmail" name="email" required
-                  placeholder="Email address of Organization, Company, Brand etc"
-                  value={supportEmail} onChange={(e) => setSupportEmail(e.target.value)}
-                />
-              </div>
-              <div className={styles.supportTextArea}>
-                <label htmlFor="supportMessage">Message/Description:</label>
-                <textarea
-                  id="supportMessage" name="message" required
-                  placeholder="E.g We will like to partner with the Klub on abcd event and we will like to receive the proposal for the event."
-                  value={supportMessage} onChange={(e) => setSupportMessage(e.target.value)}
-                >
-                </textarea>
-              </div>
-              <div aria-hidden="true">
-                <input type="hidden" name="_subject" value="Message from Contact Form on KB Website" />
-                <input type="hidden" name="_autoresponse"
-                  value="Your message has been delivered to the KB Klub team. We will get back to you as soon as possible. Thank you for reaching out to us." 
-                />
-                <input type="hidden" name="_next" value="https://kbklub.org/" />
-                <input type="hidden" name="_template" value="box" />
-              </div>
-              <button type="submit">
-                Submit
-                <FaArrowRight />
-              </button>
-            </div>
-          </form>
-        </div>
-      </main>
+            </form>
+          </div>
+        </main>
+      </div>
     </>
   );
 }
