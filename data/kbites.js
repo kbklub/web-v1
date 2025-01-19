05d7296803909ea42c6881444137cd4c45d916f4
Sodiq 'Ade' Sanusi <ademilddiq@gmail.com>
2025-01-19 23:10:16 +0100

    content: add newly-elected exec, and kbites

--- data/kbites.js
+++ data/kbites.js
@@ -1,11 +1,11 @@
 //* Import needed images first
-import keebiteImage from "./images/kbites/keebite.png";
-import curatorImage from "./images/kbites/curator.jpeg";
-import editorImage from "./images/kbites/editor.jpeg";
-import kashbiteImage from "./images/kbites/kashbite.jpeg";
-import kbriteImage from "./images/kbites/kbrite.jpeg";
-import protocolImage from "./images/kbites/protocol.jpeg";
-import viceroyImage from "./images/kbites/viceroy.jpeg";
+import keebiteImage from "./images/kbites/keebite.jpg";
+import curatorImage from "./images/kbites/curator.jpg";
+import editorImage from "./images/kbites/editor.jpg";
+import kashbiteImage from "./images/kbites/kashbite.jpg";
+import kbriteImage from "./images/kbites/kbrite.jpg";
+import protocolImage from "./images/kbites/protocol.jpg";
+import viceroyImage from "./images/kbites/viceroy.jpg";
 
 // exectiveSchema = {
 //   name: "",
@@ -15,37 +15,37 @@ import viceroyImage from "./images/kbites/viceroy.jpeg";
 
 export const kbExecutives = [
   {
-    name: "Madehin Mustapha",
+    name: "Olomiyete Ayobami",
     title: "Keebite (President)",
     image: keebiteImage,
   },
   {
-    name: "Olomiyete Ayobami",
+    name: "Saliu Ayobami",
     title: "KB Viceroy (Vice President)",
     image: viceroyImage,
   },
   {
-    name: "Onyegbula Covenant",
+    name: "Oloyede Sheriffdeen",
     title: "KBrite (General Secretary)",
     image: kbriteImage,
   },
   {
-    name: "Dere Jibril",
+    name: "Unachukwu Henry",
     title: "Kashbite (Financial Secretary)",
     image: kashbiteImage,
   },
   {
-    name: "Folly Oluwaremilekun",
+    name: "Safiriyu Kamal",
     title: "KB Protocol (Social Secretary & PRO)",
     image: protocolImage,
   },
   {
-    name: "Oladipupo Paul",
+    name: "Yussuf Al-Ameen",
     title: "KB Editor (Editor-in-chief)",
     image: editorImage,
   },
   {
-    name: "Abdulsalam Habeeb",
+    name: "Denapo Moses",
     title: "KB Curator",
     image: curatorImage,
   },
@@ -59,7 +59,15 @@ export const kbites = [
   "Okusanya Olusola",
   "Olanrewaju Oladayo",
   "Salami Akorede",
-  "Saliu Ayobami",
+  "Madehin Mustapha",
+  "Dere Jibril",
+  "Abdulsalam Habeeb",
+  "Ejodamen Joshua",
+  "Folly Oluwaremilekun",
+  "Imana Oluwaseun",
+  "Oladipupo Paul",
+  "Thanni Abdulmajeed",
+  "Onyegbula Covenant",
 ].sort();
 
 export const lifeKbites = [
